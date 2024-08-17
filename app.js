35d782ec6295d4959e29f14ebf6b285ff4a9285d
z0opreme <remy.engassac@icloud.com>
2024-08-17 16:10:15 +0200

    fix double interval after a cycle

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-let workTime = 3;
-let breakTime = 5;
+let workTime = 1800;
+let breakTime = 300; // 5 minutes
 
 function formattedTime(time) {
 	return `${Math.trunc(time/60)}:${time % 60 < 10 ? `0${time % 60}` : time % 60}`;
@@ -12,54 +12,42 @@ diplayWorkTime.textContent = formattedTime(workTime);
 diplayBreakTime.textContent = formattedTime(breakTime);
 
 const toggleBtn = document.querySelector('.toggle-time-btn');
-toggleBtn.addEventListener('click', togglePomodoro)
+toggleBtn.addEventListener('click', togglePomodoro);
 
 let currentInterval = false;
 let timerId;
+let isPaused = true;
 
 function togglePomodoro() {
-	handlePlayPause();
 	if (currentInterval) {
-		if (isPaused) {
-			timerId = setInterval(handleTicks, 1000);
-		} else {
-			clearInterval(timerId);
-		}
+		handlePlayPause();
 		return;
 	}
 
 	currentInterval = true;
 	workTime--;
 	diplayWorkTime.textContent = formattedTime(workTime);
-	timerId = setInterval(handleTicks, 1000);
+	handlePlayPause();
 }
 
-let isPaused = true;
-
 function handlePlayPause() {
-	if (toggleBtn.getAttribute('data-toggle') === 'play') {
+	if (isPaused) {
 		isPaused = false;
 		toggleBtn.firstElementChild.src = 'assets/pause.svg';
 		toggleBtn.setAttribute('data-toggle', 'pause');
-		if (workTime) {
-			handleAnimation({work : true, break : false});
-		} else {
-			handleAnimation({work : false, break : true});
-		}
+		timerId = setInterval(handleTicks, 1000);
 	} else {
 		isPaused = true;
+		clearInterval(timerId);
 		toggleBtn.firstElementChild.src = 'assets/play.svg';
 		toggleBtn.setAttribute('data-toggle', 'play');
 	}
+	handleAnimation({work: workTime > 0, break: workTime <= 0 && breakTime > 0});
 }
 
 function handleAnimation(timerState) {
 	for (const timer in timerState) {
-		if (timerState[timer]) {
-			document.querySelector(`.${timer}-title`).classList.add('active');
-		} else {
-			document.querySelector(`.${timer}-title`).classList.remove('active');
-		}
+		document.querySelector(`.${timer}-title`).classList.toggle('active', timerState[timer]);
 	}
 }
 
@@ -72,16 +60,16 @@ function handleTicks() {
 	if (workTime > 0) {
 		workTime--;
 		diplayWorkTime.textContent = formattedTime(workTime);
-		handleAnimation({work : true, break : false});
+		handleAnimation({work: true, break: false});
 	} else if (workTime === 0 && breakTime > 0) {
 		breakTime--;
 		diplayBreakTime.textContent = formattedTime(breakTime);
-		handleAnimation({work : false, break : true});
+		handleAnimation({work: false, break: true});
 	} else {
 		resetTimer();
 		cyclesCount++;
-		cycles.textContent = `Cycle(s) : ${cyclesCount}`;
-		handleAnimation({work : false, break : false});
+		cycles.textContent = `Cycle(s): ${cyclesCount}`;
+		handleAnimation({work: false, break: false});
 	}
 }
 
@@ -98,9 +86,10 @@ function resetTimer() {
 	diplayWorkTime.textContent = formattedTime(workTime);
 
 	cyclesCount = 0;
-	cycles.textContent = `Cycle(s) : ${cyclesCount}`;
+	cycles.textContent = `Cycle(s): ${cyclesCount}`;
 
 	isPaused = true;
 	toggleBtn.firstElementChild.src = 'assets/play.svg';
 	toggleBtn.setAttribute('data-toggle', 'play');
+	handleAnimation({work: false, break: false});
 }
